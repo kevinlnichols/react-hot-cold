5027d99c4ae2975e4d7ab2ec58c1049dd4713af8
Kevin Nichols <kevinlnichols@gmail.com>
2017-12-12 20:14:27 -0500

    redone with redux

--- src/components/guess-count.js
+++ src/components/guess-count.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {connnect} from 'react-redux';
 
 import './guess-count.css';
 
-export default function GuessCount(props) {
+export function GuessCount(props) {
     const isPlural = props.guessCount !== 1;
     const guessNoun = isPlural ? 'guesses' : 'guess';
 
@@ -12,3 +13,9 @@ export default function GuessCount(props) {
         </h2>
     );
 }
+
+const mapStateToProps = state => ({
+    guessCount: state.guesses.length
+});
+
+export default connnect(mapStateToProps)(GuessCount);
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux';
+
+import {game} from './reducers';
+
+export default createStore(game);
